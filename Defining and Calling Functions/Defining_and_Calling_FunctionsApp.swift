572034d7b14689b9043f495369f1c7b79d16ef07
Wongi <hpurs95@gmail.com>
2022-07-04 16:53:18 +0900

    Initial Commit

---
+++ Defining and Calling Functions/Defining_and_Calling_FunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Defining_and_Calling_FunctionsApp.swift
+//  Defining and Calling Functions
+//
+//  Created by MacBook Air on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct Defining_and_Calling_FunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
